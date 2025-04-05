b9a2cebd16335abc30308af2b921662e23fdd773
Miguelki <63244249+miguelkix30@users.noreply.github.com>
2025-04-05 16:03:25 +0200

    Update discord verification

--- src/assets/js/launcher.js
+++ src/assets/js/launcher.js
@@ -846,7 +846,7 @@ class Launcher {
         discorderrdialog.openDialog({
           title: "Verificación de Discord",
           content:
-            "Para poder acceder al launcher debes iniciar sesión con tu cuenta de Discord y estar en el servidor de OWLSAFIO. <br><br>Quieres iniciar sesión ahora?",
+            "Para poder acceder al launcher debes iniciar sesión con tu cuenta de Discord y estar en el servidor de Owleaf Studio. <br><br>Quieres iniciar sesión ahora?",
           options: true,
           callback: resolve,
         });
@@ -915,7 +915,7 @@ class Launcher {
         discorderrdialog.openDialog({
           title: "Error al verificar la cuenta de Discord",
           content:
-            "No se ha detectado que seas miembro del servidor de Discord. Para poder utilizar el launcher debes ser miembro del servidor. <br>Quieres intentarlo de nuevo?",
+            "No se ha detectado que seas miembro del servidor de Discord. Para poder utilizar el launcher debes ser miembro del servidor de Owleaf Studio. <br>Quieres intentarlo de nuevo?",
           options: true,
           callback: resolve,
         });
